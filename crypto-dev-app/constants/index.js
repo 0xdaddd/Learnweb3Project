12caaad532918bb1d99a316d8ffe51955493da89
daddd <daddd5913@gmail.com>
2023-03-16 23:47:06 +0800

    Build your own Decentralized Exchange like Uniswap

--- crypto-dev-app/constants/index.js
+++ crypto-dev-app/constants/index.js
@@ -1,12 +1,423 @@
+
+export const EXCHANGE_CONTRACT_ADDRESS = "0xD6691bca33a7cfCb595309CBdb150396CC8159c9";
 export const DAO_CONTRACT_ADDRESS = "0x1aEf09c2032b52631147F5Cf544F52714798e265";
-
 export const TOKEN_CONTRACT_ADDRESS = "0x2Bb1817F8F494F3Bc75F0438A166532252946190";
-
 export const NFT_CONTRACT_ADDRESS = "0xF870192B285E33eA63e66a8959f5443Ea0188887"
-
 export const WHITELIST_CONTRACT_ADDRESS = "0x9469e29622E7784e42a4B08e6e04AaCd65b2942b";
 
 
+export const EXCHANGE_CONTRACT_ABI = [
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "_CryptoDevtoken",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "spender",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "value",
+        "type": "uint256"
+      }
+    ],
+    "name": "Approval",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "from",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "to",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "value",
+        "type": "uint256"
+      }
+    ],
+    "name": "Transfer",
+    "type": "event"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_amount",
+        "type": "uint256"
+      }
+    ],
+    "name": "addLiquidity",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "spender",
+        "type": "address"
+      }
+    ],
+    "name": "allowance",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "spender",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "amount",
+        "type": "uint256"
+      }
+    ],
+    "name": "approve",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "account",
+        "type": "address"
+      }
+    ],
+    "name": "balanceOf",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "cryptoDevTokenAddress",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_tokensSold",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "_minEth",
+        "type": "uint256"
+      }
+    ],
+    "name": "cryptoDevTokenToEth",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "decimals",
+    "outputs": [
+      {
+        "internalType": "uint8",
+        "name": "",
+        "type": "uint8"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "spender",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "subtractedValue",
+        "type": "uint256"
+      }
+    ],
+    "name": "decreaseAllowance",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_minTokens",
+        "type": "uint256"
+      }
+    ],
+    "name": "ethToCryptoDevToken",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "inputAmount",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "inputReserve",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "outputReserve",
+        "type": "uint256"
+      }
+    ],
+    "name": "getAmountOfTokens",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "pure",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "getReserve",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "spender",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "addedValue",
+        "type": "uint256"
+      }
+    ],
+    "name": "increaseAllowance",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "name",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_amount",
+        "type": "uint256"
+      }
+    ],
+    "name": "removeLiquidity",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "symbol",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "totalSupply",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "to",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "amount",
+        "type": "uint256"
+      }
+    ],
+    "name": "transfer",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "from",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "to",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "amount",
+        "type": "uint256"
+      }
+    ],
+    "name": "transferFrom",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+];
 
 export const DAO_CONTRACT_ABI = [
 {
--- hardhat-learnweb3/scripts/deploy.js
+++ hardhat-learnweb3/scripts/deploy.js
@@ -1,10 +1,15 @@
 const { ethers } = require("hardhat");
 require("dotenv").config({ path: ".env" });
-const { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS, NFT_CONTRACT_ADDRESS} = require("../constants");
+const { 
+  DAO_CONTRACT_ADDRESS,
+  TOKEN_CONTRACT_ADDRESS, 
+  NFT_CONTRACT_ADDRESS,
+  WHITELIST_CONTRACT_ADDRESS
+} = require("../constants");
 
 // async function main() {
 //   // Address of the Crypto Devs NFT contract that you deployed in the previous module
-//   const cryptoDevsNFTContract = CRYPTO_DEVS_NFT_CONTRACT_ADDRESS;
+//   const cryptoDevsNFTContract = NFT_CONTRACT_ADDRESS;
 
 //   /*
 //     A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
@@ -28,33 +33,52 @@ const { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS, NFT_CONTRACT_ADDRESS} = require("../co
 //   // Crypto Devs Token Contract Address: 0x2Bb1817F8F494F3Bc75F0438A166532252946190
 // }
 
+// async function main() {
+//   // Deploy the CryptoDevNFTMarketplace contract first
+//   const CryptoDevNFTMarketplace = await ethers.getContractFactory(
+//     "CryptoDevNFTMarketplace"
+//   );
+//   const cryptoDevNFTMarketplace = await CryptoDevNFTMarketplace.deploy();
+//   await cryptoDevNFTMarketplace.deployed();
+
+//   console.log("CryptoDevNFTMarketplace deployed to: ", cryptoDevNFTMarketplace.address);
+
+//   // Now deploy the CryptoDevsDAO contract
+//   const CryptoDevsDAO = await ethers.getContractFactory("CryptoDevsDAO");
+//   const cryptoDevsDAO = await CryptoDevsDAO.deploy(
+//     cryptoDevNFTMarketplace.address,
+//     NFT_CONTRACT_ADDRESS,
+//     {
+//       // This assumes your metamask account has at least 1 ETH in its account
+//       // Change this value as you want
+//       value: ethers.utils.parseEther("0.2"),
+//     }
+//   );
+//   await cryptoDevsDAO.deployed();
+
+//   console.log("CryptoDevsDAO deployed to: ", cryptoDevsDAO.address);
+
+//   // CryptoDevNFTMarketplace deployed to:  0x934BcA5a436B52E2f1A59669aF926c370Babd6FF
+//   // CryptoDevsDAO deployed to:  0x1aEf09c2032b52631147F5Cf544F52714798e265
+// }
+
 async function main() {
-  // Deploy the CryptoDevNFTMarketplace contract first
-  const CryptoDevNFTMarketplace = await ethers.getContractFactory(
-    "CryptoDevNFTMarketplace"
+  const cryptoDevTokenAddress = TOKEN_CONTRACT_ADDRESS;
+  /*
+  A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+  so exchangeContract here is a factory for instances of our Exchange contract.
+  */
+  const exchangeContract = await ethers.getContractFactory("Exchange");
+
+  // here we deploy the contract
+  const deployedExchangeContract = await exchangeContract.deploy(
+    cryptoDevTokenAddress
   );
-  const cryptoDevNFTMarketplace = await CryptoDevNFTMarketplace.deploy();
-  await cryptoDevNFTMarketplace.deployed();
+  await deployedExchangeContract.deployed();
 
-  console.log("CryptoDevNFTMarketplace deployed to: ", cryptoDevNFTMarketplace.address);
-
-  // Now deploy the CryptoDevsDAO contract
-  const CryptoDevsDAO = await ethers.getContractFactory("CryptoDevsDAO");
-  const cryptoDevsDAO = await CryptoDevsDAO.deploy(
-    cryptoDevNFTMarketplace.address,
-    NFT_CONTRACT_ADDRESS,
-    {
-      // This assumes your metamask account has at least 1 ETH in its account
-      // Change this value as you want
-      value: ethers.utils.parseEther("0.2"),
-    }
-  );
-  await cryptoDevsDAO.deployed();
-
-  console.log("CryptoDevsDAO deployed to: ", cryptoDevsDAO.address);
-
-  // CryptoDevNFTMarketplace deployed to:  0x934BcA5a436B52E2f1A59669aF926c370Babd6FF
-  // CryptoDevsDAO deployed to:  0x1aEf09c2032b52631147F5Cf544F52714798e265
+  // print the address of the deployed contract
+  console.log("Exchange Contract Address:", deployedExchangeContract.address);
+  //Exchange Contract Address: 0xD6691bca33a7cfCb595309CBdb150396CC8159c9
 }
 
 // Call the main function and catch if there is any error
